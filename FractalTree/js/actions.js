82cf808e6e918eb40a02a8ca558c8e48cdc761dd
Rheriel <rheriel@hotmail.co.jp>
2017-01-29 15:08:04 -0600

    Created a simple Fractal Tree.

--- FractalTree/js/actions.js
+++ FractalTree/js/actions.js
@@ -3,5 +3,35 @@ function setup(){
 }
 
 function draw(){
-	background(0);
+	var angle = PI / 4;
+	var length = 100;
+	var offset = .6;
+
+	background(51);
+	stroke(255);
+	translate(400, height);
+	drawTrunk(length);
+	push();
+	branch(length, offset, angle);
+	pop();
+	branch(length, offset, -angle);
+	
+}
+
+function branch(len, off, ang){
+	if (len > 4) {
+		rotate(ang);
+		line(0, 0, 0, -len);
+		translate(0, -len);
+		push();
+		branch(len * off, off, ang);
+		pop();
+		branch(len  * off, off,  -ang);
+	}
+}
+
+function drawTrunk(len){
+	line(0,0,0,-len);
+	translate(0,-len);
+	push();
 }
